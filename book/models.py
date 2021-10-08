ee994e8ce2a5999c5e40f0c16925724ecb7a8fc7
Coder-Aht007 <bsef17a007@pucit.edu.pk>
2021-10-08 19:13:38 +0500

    added profiler, utils for testing the queries

---
+++ book/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=100)
+    price = models.IntegerField(default=0)
+    publisher = models.ForeignKey(
+        Publisher, on_delete=models.CASCADE, related_name='books')
+
+    def __str__(self):
+        return self.name
+
+
+class Store(models.Model):
+    name = models.CharField(max_length=300)
+    books = models.ManyToManyField(Book, related_name='store')
+
+    def __str__(self):
+        return self.name
---
+++ book/utils.py
@@ -0,0 +1,57 @@
+from .decorators import query_debugger
+from .models import Book, Store
+
+
+@query_debugger
+def book_list():
+
+    queryset = Book.objects.all()
+
+    books = []
+    for book in queryset:
+        books.append({'id': book.id, 'name': book.name,
+                     'publisher': book.publisher.name})
+
+    return books
+
+
+@query_debugger
+def book_list_select_related():
+
+    queryset = Book.objects.select_related('publisher').all()
+
+    books = []
+
+    for book in queryset:
+        books.append({'id': book.id, 'name': book.name,
+                     'publisher': book.publisher.name})
+
+    return books
+
+
+@query_debugger
+def store_list_prefetch_related():
+
+    queryset = Store.objects.prefetch_related('books')
+
+    stores = []
+
+    for store in queryset:
+        books = [book.name for book in store.books.all()]
+        stores.append({'id': store.id, 'name': store.name, 'books': books})
+
+    return stores
+
+
+@query_debugger
+def store_list():
+
+    queryset = Store.objects.all()
+
+    stores = []
+
+    for store in queryset:
+        books = [book.name for book in store.books.all()]
+        stores.append({'id': store.id, 'name': store.name, 'books': books})
+
+    return stores
